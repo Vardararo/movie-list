8749612980cfdb052c690c79e9190f032f6155be
Edib H <edib.hafizovic@gmail.com>
2024-06-11 15:10:02 +0200

    add comments

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ import os
 
 load_dotenv(".env")
 
+# Pulling movie data from The Movie Database using the TMDB API
 TMDB_API_KEY = os.environ.get("TMDB_API_KEY")
+
+# URL for respective movie's poster
 MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/original"
 
 year = datetime.now().year
@@ -22,11 +25,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("APP_CONFIG")
 Bootstrap5(app)
 
+# Flask Form used when adding a new movie or editing the review of an existing movie
 class EditForm(FlaskForm):
     rating_edit = StringField(label='Your Rating Out of 10', render_kw={"autocomplete":"off"})
     review_edit = StringField(label='Your Review', render_kw={"autocomplete":"off"})
     submit = SubmitField("Done")
-    
+
+# Flask Form used to search TMDB using movie title    
 class FindMovieForm(FlaskForm):
     title = StringField("Movie Title", validators=[DataRequired()], render_kw={"autocomplete":"off"})
     submit = SubmitField("Add Movie")
@@ -34,6 +39,7 @@ class FindMovieForm(FlaskForm):
 class Base(DeclarativeBase):
     pass
 
+# Database used to store movie data
 db = SQLAlchemy(model_class=Base)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movie_coll.db"
 db.init_app(app)
@@ -51,10 +57,11 @@ class Movie(db.Model):
 with app.app_context():
     db.create_all()
 
-# with app.app_context():
-#     Movie.query.filter(Movie.id == 3).delete()
-#     db.session.commit()
+# #with app.app_context():
+# #    Movie.query.filter(Movie.id == 3).delete()
+# #    db.session.commit()
 
+# Home page showing the watched movies in order of their respective scores (highest to lowest)
 @app.route("/", methods=["GET", "POST"])
 def home():
     collection = db.session.execute(db.select(Movie).order_by(Movie.rating.desc()))
@@ -68,7 +75,7 @@ def home():
     
     return render_template("index.html", movies = all_movies, copyright_year = year)
 
-
+# Edit page used for altering the score and review of a movie already in database
 @app.route("/edit", methods=["GET", "POST"])
 def edit():
     form = EditForm()
@@ -86,7 +93,7 @@ def edit():
     
     return render_template("edit.html", movie=movie, form=form, copyright_year = year)
 
-
+# Function to remove a movie from our database
 @app.route("/delete")
 def delete():
     movie_id = request.args.get("id")
@@ -102,7 +109,7 @@ headers = {"accept": "application/json",
             "Authorization": f"Bearer {auth}"
             }
 
-
+# Search page - loads all movies found on TMDB based on title input
 @app.route("/add", methods=["GET", "POST"])
 def add_movie():
     form = FindMovieForm()
@@ -115,7 +122,7 @@ def add_movie():
     
     return render_template("add.html", form=form, copyright_year = year)
 
-
+# Function to add the selected movie to the database
 @app.route("/find")
 def find_movie():
     movie_api_id = request.args.get("id")
