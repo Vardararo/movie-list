515f700125edb1923c26b1082b57582da735c1d7
Edib H <edib.hafizovic@gmail.com>
2024-06-11 20:57:52 +0200

    fix Text type

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ class Movie(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     year: Mapped[int] = mapped_column(Integer, nullable=False)
-    description: Mapped[str] = mapped_column(Text(1000), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
     rating: Mapped[float] = mapped_column(Float, nullable=True)
     ranking: Mapped[int] = mapped_column(Integer, nullable=True)
     review: Mapped[str] = mapped_column(String(250), nullable=True)
